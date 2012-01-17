81c23828c665852c924cea0b0edf7735cac978fa
Dan Neff <dneff@adobe.com>
2012-01-17 08:26:02 -0800

    solved 12 and learned about code profiling and bitmasking

--- p012.py
+++ p012.py
@@ -29,9 +29,16 @@
 #
 # Possible insight:
 #  Need at least 9 prime factors to have 2^9 possible divisors 
+#  Need to create a prime factors array for each number for lookup purposes
 #  
 
 import math,sys
+# handy for profiling
+# import cProfile
+# cProfile.run('solution()')
+
+primes = [2]
+factor_dict = {}
 
 def bitlist(number,size):
   if number.bit_length() > size: 
@@ -46,37 +53,47 @@ def add(x,y):
 def multiply(x,y):
   return x * y
 
-# DTN - improve here. Too slow
+# corrected function.
+# show more care/focus.
 def triangle(x):
-  return reduce(add,range(1,x+1))
+  return (x * (x + 1))/2 
 
 def smallest_factor(x):
-  if is_prime(x):
-    return x
-  for product in range(2,math.trunc(x/2) + 1):
-    if x % product == 0:
-      return product
+  if x in factor_dict: return factor_dict[x][0]
+  if is_prime(x): return x
+  for prime in primes:
+    if x % prime == 0: return prime
+
+def is_prime(x):
+  if x in factor_dict: return False 
+  if x in primes: return True
+  max = math.trunc(x/2)
+  num_range = [p for p in primes if p < max]
+  while num_range:
+    if x % num_range.pop(0) == 0: return False
+  primes.append(x)
+  return True
 
 def factors(x):
+  if x in factor_dict: return factor_dict[x]
   results = []
   if is_prime(x):
     results.append(x)
     return results
-  results.append(smallest_factor(x))
-  f2 = x/smallest_factor(x)
-  if is_prime(f2):
-    results.append(f2)
+  f1 = smallest_factor(x)
+  results.append(f1)
+  f2 = x/f1
+  if is_prime(f2): results.append(f2)
   else:
     for ff in factors(f2):
       results.append(ff)
+  factor_dict[x] = results
   return results
 
-def is_prime(x):
-  for product in range(2, math.trunc(x/2) + 1):
-    if x % product == 0:
-        return False
-  return True
-
+# insight - if you have an array, you can use a 
+# bit array of the same length to help find all
+# the combinations of elements.
+# came to me while driving with my daughter (AFK).
 def divisors(prime_list):
   divisor_list = []
   for combo in range(1,2**len(prime_list)):
@@ -85,12 +102,19 @@ def divisors(prime_list):
   divisor_list.append(1)
   return sorted(list(set(divisor_list)))
 
-x = 1
-while True:
-  print len(divisors(factors(triangle(x))))
-  if len(divisors(factors(triangle(x)))) > 500:
-    print x
-    print triangle(x)
-    print divisors(factors(triangle(x)))
-    sys.exit(0)
-  x += 1
+# by making a solution function, you can use the code profiler
+# much easier
+def solution():
+  x = 2
+  while True:
+    candidate = factors(triangle(x))
+    # need at least 9 factors
+    if len(candidate) > 8: 
+      if len(divisors(candidate)) > 500:
+        return reduce(multiply,candidate)
+    x += 1
+
+# real	0m25.564s
+# user	0m25.526s
+# sys	0m0.016s
+print solution()
