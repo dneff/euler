a5a30f3949ad04aacd781c071951ff9c7c34dd6d
Dan Neff <dneff@adobe.com>
2012-08-26 18:36:36 -0700

    insanely satisfying. To test the plaintext decrypt, I chose to test for the string ' the '. There was only one decrypted solution which contained that string. Probably more sane to use some alphanumeric character space check. Might circle back for another run at this in the future.

---
+++ solved/p059.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+# Dan Neff - (dan@neff.cc)
+# Each character on a computer is assigned a unique code and
+# the preferred standard is ASCII (American Standard Code for
+# Information Interchange). For example, uppercase A = 65,
+# asterisk (*) = 42, and lowercase k = 107.
+#
+# A modern encryption method is to take a text file, convert the
+# bytes to ASCII, then XOR each byte with a given value, taken
+# from a secret key. The advantage with the XOR function is that
+# using the same encryption key on the cipher text, restores the
+# plain text; for example, 65 XOR 42 = 107, then 107 XOR 42 = 65.
+#
+# For unbreakable encryption, the key is the same length as the
+# plain text message, and the key is made up of random bytes. The
+# user would keep the encrypted message and the encryption key in
+# different locations, and without both "halves", it is impossible
+# to decrypt the message.
+#
+# Unfortunately, this method is impractical for most users, so the
+# modified method is to use a password as a key. If the password is
+# shorter than the message, which is likely, the key is repeated
+# cyclically throughout the message. The balance for this method
+# is using a sufficiently long password key for security, but short
+# enough to be memorable.
+#
+# Your task has been made easy, as the encryption key consists of three
+# lower case characters. Using cipher1.txt (right click and 
+# 'Save Link/Target As...'), a file containing the encrypted ASCII codes,
+# and the knowledge that the plain text must contain common English words,
+# decrypt the message and find the sum of the ASCII values in the
+# original text.
+#
+import math,sys,string
+
+def xor_translate(cipher,key):
+  answer = []
+  for i in range(0,len(cipher)):
+    key_position = (i % len(key))
+    answer.append(chr(cipher[i] ^ ord(key[key_position])))
+  return ('').join(answer)
+
+  return answer
+
+fh = open('./p059-cipher1.txt','r')
+
+cipher = map(string.strip,fh.readlines())
+cipher = [int(x) for x in cipher[0].split(',')]
+
+solution = ''
+
+for a in map(chr, range(97,123)):
+  for b in map(chr, range(97,123)):
+    for c in map(chr, range(97,123)):
+      passphrase = [a, b, c]
+      candidate = xor_translate(cipher,passphrase)
+      if " the " in candidate.lower():
+        solution = candidate
+
+print 'solution: ',sum([ord(x) for x in list(solution)])
